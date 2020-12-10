ce07af4a1dabf0b6ea5dfb126c28cb72ab2d3a20
Sourabh Thakur <sourabhthakurbhl@gmail.com>
2020-12-10 12:24:55 +0530

    added collision type for Zr and the related data

--- Monte_Carlo.py
+++ Monte_Carlo.py
@@ -10,6 +10,8 @@ from collision import get_collision_prob_dict,elastic_collision_energy,inelastic
 DATA_DIRECTORY_NUCLEUS = '/home/sourabh/Development/NUCLEAR_PROJECT/data'
 DATA_DIRECTORY_D2O = '/home/sourabh/Development/NUCLEAR_PROJECT/data/D2O_collision'
 DATA_DIRECTORY_U_238 = '/home/sourabh/Development/NUCLEAR_PROJECT/data/U_238_collision'
+DATA_DIRECTORY_Zr = '/home/sourabh/Development/NUCLEAR_PROJECT/data/Zr_collision'
+
 class MultiplicationFactor:
 
     def __init__(self,steps):
@@ -19,6 +21,7 @@ class MultiplicationFactor:
         self.nucleus_prob_dataframe = get_dataframes(DATA_DIRECTORY_NUCLEUS)
         self.D2O_dataframe = get_dataframes(DATA_DIRECTORY_D2O)
         self.U_238_dataframe = get_dataframes(DATA_DIRECTORY_U_238)
+        self.Zr_dataframe = get_dataframes(DATA_DIRECTORY_Zr)
         print("Reading data done")
 
     @staticmethod
@@ -59,9 +62,12 @@ class MultiplicationFactor:
             col_prob_dict = get_collision_prob_dict(init_energy, self.D2O_dataframe)
             return self.generate_choices_probability(init_energy,nuclei_prob_dict=col_prob_dict)[0]
         if nuclei=='U_238':
-            print('U_238_milaa re baba')
+            # print('U_238_milaa re baba')
             col_prob_dict = get_collision_prob_dict(init_energy, dataframe=self.U_238_dataframe, nucleus='U_238')
             return self.generate_choices_probability(init_energy,nuclei_prob_dict=col_prob_dict)[0]
+        if nuclei=='Zr':
+            col_prob_dict = get_collision_prob_dict(init_energy, dataframe=self.Zr_dataframe,nucleus='Zr')
+            return self.generate_choices_probability(init_energy, nuclei_prob_dict=col_prob_dict)[0]
         return 'in_progress'
 
     @staticmethod
@@ -77,26 +83,6 @@ class MultiplicationFactor:
 if __name__ == "__main__":
 
     Simulation_Instance = MultiplicationFactor(10000)
-    # print(Simulation_Instance.pdf(0))
-    # i = 0
-    # lst = []
-    # population_list = [0.000007*i for i in range(1,1000000)]
-    # # print(population[0:20])
-    # weights_list = [Simulation_Instance.pdf(step) for step in population_list]
-    # # print(weights[0:20])
-    # print()
-
-    # while i != 1000:
-    #     lst.append(Simulation_Instance.generate_random_with_dist(population_list, weights_list))
-    #     if i % 100 == 0:
-    #         print(str(i) + "iterations done")
-    #     i = i + 1
-
-
-    # print(lst)
-    # plt.clf()
-    # sns.distplot(lst)
-    # plt.xlim(0, 8)
 
     number_of_nuetrons = int(input('Please enter number of nuetrons: '))
     
@@ -104,24 +90,31 @@ if __name__ == "__main__":
     nuetron_energies = [Simulation_Instance.fix_init_nuetron_energy() for count_nuetron in range(number_of_nuetrons)]
     print('Done. Starting Simulation now')
     fission_count = 0
+    print(nuetron_energies)
     for nuetron_energy in nuetron_energies:
         # nuetron_energy = Simulation_Instance.fix_init_nuetron_energy()
         # print(nuetron_energy)
         
         is_alive_nuetron = True
         # nucleus_prob = 'U_238'
+        
         while(is_alive_nuetron):
             print(nuetron_energy)
             if(nuetron_energy<0.00005):
                 is_alive_nuetron = False
                 continue
             nucleus_prob = Simulation_Instance.generate_choices_probability(nuetron_energy)[0]
-            # nucleus_prob = 'U_238'
+            # nucleus_prob = 'U_235'
             # print(nucleus_prob)
             if nucleus_prob == 'Zr':
                 print(nucleus_prob)
-                nuetron_energy = Simulation_Instance.energy_post_collision(nuetron_energy,'inelastic','Zr')
-                continue
+                collision_type = Simulation_Instance.generate_collision_type(nuetron_energy,nucleus_prob)
+                if collision_type == 'capture':
+                    is_alive_nuetron = False
+                    continue
+                nuetron_energy = Simulation_Instance.energy_post_collision(nuetron_energy,collision_type,'Zr')
+                print(nuetron_energy)
+
             if nucleus_prob == 'U_235':
                 print(nucleus_prob)
                 fission_count += 1
--- collision.py
+++ collision.py
@@ -9,12 +9,15 @@ ATOMIC_NUMBER = {
 }
 ELASTIC_ENERGY_LOSS_COEFF = {
     'D2O': 0.491,
-    'U_238':0.992
+    'U_238':0.992,
+    'Zr':0.956
 }
 D2O_data_directory = '/home/sourabh/Development/NUCLEAR_PROJECT/data/D2O_collision'
 U_238_directory = '/home/sourabh/Development/NUCLEAR_PROJECT/data/U_238_collision'
-D2O_nuclei_type = ['D_inelastic', 'O_elastic', 'D_elastic', 'O_inelastic']
+D2O_nuclei_type = ['D_inelastic', 'O_elastic', 'D_elastic', 'O_inelastic','D_capture','O_capture']
 U_238_nuclei_type = ['U_238_inelastic','U_238_elastic', 'U_238_capture']
+Zr_nuclei_type = ['Zr_inelastic', 'Zr_elastic', 'Zr_capture']
+Zr_directory = '/home/sourabh/Development/NUCLEAR_PROJECT/data/Zr_collision'
 
 def elastic_collision_energy(init_energy, nucleus):
     multiplier = ELASTIC_ENERGY_LOSS_COEFF[nucleus]
@@ -31,22 +34,35 @@ def get_D2O_cross_section(energy,dataframe):
     D2O_cross_section_dict = {}
     D2O_cross_section_dict['elastic'] = 2*cross_section_dict['D_elastic'] + cross_section_dict['O_elastic']
     D2O_cross_section_dict['inelastic'] = 2*cross_section_dict['D_inelastic'] + cross_section_dict['O_inelastic']
+    D2O_cross_section_dict['capture'] = 2*cross_section_dict['O_capture'] + cross_section_dict['O_capture']
     return D2O_cross_section_dict
 
-def get_U_238_cross_section(energy):
-    cross_section_dict = get_cross_section_dict(energy, directory=U_238_directory,Nuclei=U_238_nuclei_type)
+def get_U_238_cross_section(energy,dataframe):
+    cross_section_dict = get_cross_section_dict(energy, directory=U_238_directory,Nuclei=U_238_nuclei_type,dataframe=dataframe)
     U_238_cross_section_dict = {}
     U_238_cross_section_dict['elastic'] = cross_section_dict['U_238_elastic']
     U_238_cross_section_dict['inelastic'] = cross_section_dict['U_238_inelastic']
     U_238_cross_section_dict['capture'] = cross_section_dict['U_238_capture']
     return U_238_cross_section_dict
 
+def get_Zr_cross_section(energy,dataframe):
+    cross_section_dict = get_cross_section_dict(energy, directory=Zr_directory, Nuclei=Zr_nuclei_type,dataframe=dataframe)
+    Zr_cross_section_dict = {}
+    Zr_cross_section_dict['elastic'] = cross_section_dict['Zr_elastic']
+    Zr_cross_section_dict['inelastic'] = cross_section_dict['Zr_inelastic']
+    Zr_cross_section_dict['capture'] = cross_section_dict['Zr_capture']
+    return Zr_cross_section_dict
+
+
 def get_collision_prob_dict(energy,dataframe,nucleus='D2O'):
     probab_dict = {}
+    cross_section_dict = {}
     if nucleus=='D2O':
         cross_section_dict = get_D2O_cross_section(energy,dataframe)
-    else:
-        cross_section_dict = get_U_238_cross_section(energy)
+    if nucleus=='U_238':
+        cross_section_dict = get_U_238_cross_section(energy,dataframe)
+    if nucleus=='Zr':
+        cross_section_dict = get_Zr_cross_section(energy,dataframe)
 
     collision_type = cross_section_dict.keys()
     cross_section_list = [cross_section_dict[key] for key in cross_section_dict.keys()]
